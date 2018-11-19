9834ef9e02e4e60fdf70367a088c33ab6fb27e9a
Michael Luo <mluo1@stuy.edu>
2018-11-19 00:13:33 -0500

    finished

--- main.c
+++ main.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
+#include <fcntl.h>
 int populate(){
     int i = 0;
     int rando = open("/dev/random", O_RDONLY);
     int reada = read(rando,&i,sizeof(int));
     close(rando);
-    return i;
-}
+    if(i < 0){
+	i = i * -1;
+	}
+    return (i % 16) + 5;
+	}
+
+
+
 
 int main(){
-    //int mama = getpid();
-    fork();
-    printf("PID %d A: %d\n",getpid(),populate());
-}
+    
+    printf("LEGO\n");
+
+    int Orig = getpid();
+    int ChildOne = fork();
+    int ChildTwo;
+    int status;
+    int random = populate();
+
+    //printf("LEGO\n");
+	
+    if(Orig == getpid()){
+	ChildTwo = fork();
+	}
+    
+    if(Orig != getpid()){
+	printf("Child PID- %d\n",getpid());
+	//int random = populate();
+	sleep(random);
+	printf("Child PID- %d  Slept for %d Seconds\n",getpid(),random);
+	
+	}
+	
+    else if(Orig == getpid()){
+	int Orig2 = wait(&status);
+	printf("My son %d slept for %d seconds\n",Orig2,random);
+	printf("My turn to sleep\n");
+	exit(0);
+	}
+
+	return 0;
+	}
